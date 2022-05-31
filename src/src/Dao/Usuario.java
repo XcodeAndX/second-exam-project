81d5ecd8e2816f7ddaf89be34d9495e6a98125c1
Leonardo Andrés Gayón Espinel <leonardo.andresin@gmail.com>
2022-05-30 20:14:23 -0500

    registro
    creacion de registro, diseño registro

--- src/src/Dao/Usuario.java
+++ src/src/Dao/Usuario.java
@@ -1,5 +1,7 @@
 package Dao;
 
+import java.util.Objects;
+
 public class Usuario {
     int id;
     String username;
@@ -9,12 +11,14 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(int id, String username, String pass, String email) {
-        this.id = id;
+    public Usuario( String username, String pass, String email) {
+
         this.username = username;
         this.pass = pass;
         this.email = email;
     }
+
+
 
     public int getId() {
         return id;
@@ -56,5 +60,22 @@ public class Usuario {
                 ", pass='" + pass + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 19 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public String guardarUsuario(){
+        // guardar en la base de datos
+        return "El usuario ha sido guardado en la base de datos";
     }
 }
---
+++ web-inf/GuardarUsuario.jsp
@@ -0,0 +1,16 @@
+<%@ page import="Dao.Usuario" %>
+<%@ page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%
+    String username = request.getParameter("username");
+    String email = request.getParameter("email");
+    String password = request.getParameter("pass");
+
+
+    Usuario usuario = new Usuario(username, email, password);
+
+    String msg = usuario.guardarUsuario();
+%>
+<html>
+<head>
+</head>
